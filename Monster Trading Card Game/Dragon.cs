78d9eca6017ceea4ab675e02f27bb070bf000bff
Chahed Rajoub <if19b166@technikum-wien.at>
2020-11-05 09:51:35 +0100

    update cards classes

---
+++ Monster Trading Card Game/Dragon.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Monster_Trading_Card_Game
+{
+    class Dragon : SuperMonsterCard
+    {
+        public Dragon() : base("Dragon", "Monster", 11, 5, false, new[] { Element.Fire, Element.Normal, Element.Water })
+        { 
+        }
+    }
+
+    class Goblin: SuperMonsterCard
+    {
+        public Goblin() : base("Goblin", "Monster", 12, 2, false, new[] { Element.Fire, Element.Normal, Element.Water })
+        {
+        }
+    }
+    class Kraken : SuperMonsterCard
+    {
+        public Kraken() : base("Kraken", "Monster", 13, 4, false, new[] { Element.Fire, Element.Normal, Element.Water })
+        {
+        }
+    }
+    class Knight : SuperMonsterCard
+    {
+        public Knight() : base("Knight", "Monster", 14, 10, false, new[] { Element.Fire, Element.Normal, Element.Water })
+        {
+        }
+    }
+}
