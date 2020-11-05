37a30879c712bb0adced47c825afa2ae1cb85cd4
Chahed Rajoub <if19b166@technikum-wien.at>
2020-11-05 09:57:18 +0100

    Adding HTTP/REST WEbservices Project on Development

---
+++ REST_HTTP-based plain-text Webservices/REST_HTTP-based plain-text Webservices/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace REST_HTTP_based_plain_text_Webservices
+{
+   public class Server
+    {
+        
+
+        public static void Main()
+        {
+
+            string ip = "127.0.0.1"; // localhost
+            int port = 8000;
+            var server = new TcpListener(IPAddress.Parse(ip), port);
+
+            server.Start();
+            Console.WriteLine("Server has started on {0}:{1}, Waiting for a connection...", ip, port);
+
+            while (true) {
+
+                Thread.Sleep(5000); // 2 sec so the server is up and running
+
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("A client connected.");
+                NetworkStream stream = client.GetStream();
+                StreamWriter writer = new StreamWriter(stream);
+                StreamReader reader = new StreamReader(stream);
+                writer.AutoFlush = true;
+
+                
+                try
+                {
+                    Thread ClientThread = new Thread(() => getMessage(client, stream));
+                    ClientThread.Start();
+
+                    ClientThread.Join();
+
+                }
+                catch (Exception exc)
+                {
+                    Console.WriteLine("error occurred: " + exc.Message);
+                }
+
+
+            }
+
+        }
+
+
+        private static void getMessage(TcpClient client, NetworkStream stream )
+        {
+            string data;
+
+            byte[] bytes = new byte[client.Available];
+            stream.Read(bytes, 0, client.Available);
+            data = Encoding.UTF8.GetString(bytes);
+            Console.WriteLine(data);
+            RequestContent request = new RequestContent(data);
+            request.Requesthandler();
+
+        }
+    }
+}
---
+++ REST_HTTP-based plain-text Webservices/REST_HTTP-based plain-text Webservices/RequestContent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
+
+namespace REST_HTTP_based_plain_text_Webservices
+{
+   public class RequestContent
+    {
+        private string Method;
+        private string Path;
+        private string HttpVersion;
+        private string Header;
+        private Dictionary<string, string> KeyValues = new Dictionary<string, string>();
+
+        public RequestContent() { }//default empty constructor
+        public RequestContent(string data)
+        {
+
+        }
+
+        internal void Requesthandler()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
