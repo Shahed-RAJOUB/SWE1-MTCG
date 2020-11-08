2044d8c146bd077bbc15b0a56ea42c7cc3b927f1
Chahed Rajoub <if19b166@technikum-wien.at>
2020-11-08 20:47:41 +0100

    Adding NUnit testing project to test RestWebServer

---
+++ REST_HTTP-based plain-text Webservices/NUnitTestHttpServer/UnitTestResponse.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace NUnitTestHttpServer
+{
+    public class ResponseTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ REST_HTTP-based plain-text Webservices/NUnitTestHttpServer/UnitTestServer.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace NUnitTestHttpServer
+{
+    public class ServerTests
+    {
+        ServerTests test1;
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
